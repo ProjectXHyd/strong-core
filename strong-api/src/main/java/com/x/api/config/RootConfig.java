12c72413ba9c9bee96e1d4c9bc1abdbfee0ca54a
sanjeev13 <sanjeev131090@gmail.com>
2018-03-02 00:56:52 +0530

    Added swagger with UI

--- strong-api/src/main/java/com/x/api/config/RootConfig.java
+++ strong-api/src/main/java/com/x/api/config/RootConfig.java
@@ -3,10 +3,20 @@ package com.x.api.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.x.api" })
-public class RootConfig {
+public class RootConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
